a8cced6617ae4a123b1c98c2376560e0c44c715d
Adam Worede <adamg2609@gmail.com>
2026-01-12 22:24:05 +0000

    initial deployment complete

--- scripts/deploy_lambda.sh
+++ scripts/deploy_lambda.sh
@@ -5,19 +5,41 @@ set -euo pipefail
 
 # configuration
 SERVICE_NAME=$2
-CODE_PATH=$4
-ENVIRONMENT=$6
+ENVIRONMENT=$4
 
+WORK_DIR="$(pwd)"
 AWS_PROFILE="mlops_developer"
 
 echo "This is the name of the lambda function: $SERVICE_NAME"
-echo "This is the path to the function code: $CODE_PATH"
 echo "This is the environment: $ENVIRONMENT"
 
 AWS_PROFILE="mlops_developer"
 AWS_REGION="eu-north-1"
-
-STACK_NAME="$ENVIRONMENT-$SERVICE_NAME-platform-lambda-function"
+STACK_NAME="$ENVIRONMENT-lambda-functions"
 echo $STACK_NAME
 
-TEMPLATE_FILE="../SAM/lambda-function-template.yaml"
+TEMPLATE_FILE="$WORK_DIR/SAM/lambda-function-template.yaml"
+
+echo "Checking AWS identity..."
+aws sts get-caller-identity --profile $AWS_PROFILE
+
+echo "Checking SAM is installed..."
+sam --version || {
+    echo "SAM is not installed"
+    exit 1
+}
+
+echo "Building SAM template..."
+sam build --template-file  "$TEMPLATE_FILE"
+echo "SAM template built successfully."
+
+echo "Deploying SAM template..."
+sam deploy \
+    --profile $AWS_PROFILE \
+    --region $AWS_REGION \
+    --template-file "$TEMPLATE_FILE" \
+    --stack-name $STACK_NAME \
+    --parameter-overrides \
+    ServiceFunctionName="$SERVICE_NAME" \
+    Environment="$ENVIRONMENT"
+echo "SAM deployment completed successfully!"
