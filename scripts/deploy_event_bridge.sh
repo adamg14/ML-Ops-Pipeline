bfefbd31197a657a25415d4625510a11c0e0942d
Adam Worede <adamg2609@gmail.com>
2026-02-02 22:21:04 +0000

    additional changes 2

---
+++ scripts/deploy_event_bridge.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# when common errors occur, the script will immediately fail
+set -euo pipefail
+
+# configuration
+ENVIRONMENT=$2
+
+WORK_DIR="$(pwd)"
+AWS_PROFILE="mlops_developer"
+
+AWS_REGION="eu-north-1"
+STACK_NAME="$ENVIRONMENT-event-bridge"
+echo $STACK_NAME
+
+TEMPLATE_FILE="$WORK_DIR/SAM/event-bridge-template.yml"
+
+echo "Checking SAM is installed..."
+sam --version || {
+    echo "SAM is not installed"
+    exit 1
+}
+
+echo "Building SAM template..."
+sam build --template-file  "$TEMPLATE_FILE"
+echo "SAM template built successfully."
+
+echo "Deploying SAM template..."
+sam deploy \
+    --profile $AWS_PROFILE \
+    --region $AWS_REGION \
+    --template-file "$TEMPLATE_FILE" \
+    --stack-name $STACK_NAME \
+    --parameter-overrides \
+    Environment="$ENVIRONMENT"
+echo "SAM deployment completed successfully!"
